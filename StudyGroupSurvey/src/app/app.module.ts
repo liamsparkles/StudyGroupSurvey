8794476f768848e1a9fa673e26abe10b3445dce2
Liam Sparling <las18@sfu.ca>
2022-03-19 17:36:13 -0700

    Added Angular Project
    Created general multiple choice options

---
+++ StudyGroupSurvey/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatRadioModule } from '@angular/material/radio'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatRadioModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+}
+
